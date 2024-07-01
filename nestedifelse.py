20e70d8f05464a32ca91e45dee5fc6cceb0821ad
Ch3tna <chetnatanganiya@gmail.com>
2024-07-01 14:28:38 +0530

    Update nestedifelse.py

--- nestedifelse.py
+++ nestedifelse.py
@@ -2,52 +2,52 @@ print("Which meal do you want?")
 meal = input("Lunch(L) Breakfast(B) Dinner(D): ")
 
 if meal == "L":
-    food = input("German(G) or Arabian(A): ")
-    if food == "G":
-        choice = input("Döner or Brot (enter full name case sensitive): ")
-        if choice == "Döner" or choice == "Brot":
+    food = input("North Indian (NI) or South Indian (SI): ")
+    if food == "NI":
+        choice = input("Chole Puri or rajma chawal enter full name case sensitive): ")
+        if choice == "Chole puri" or choice == "Rajma chawal":
             print("You got", choice)
         else:
             print("Eat dirt")
-    elif food == "A":
-        choice = input("Shawarma or Mandi (enter full name case sensitive): ")
-        if choice == "Shawarma" or choice == "Mandi":
+    elif food == "SI":
+        choice = input("Rasam rice or Chicken curry (enter full name case sensitive): ")
+        if choice == "Rasam rice" or choice == "Chicken curry":
             print("You got", choice)
         else:
             print("Eat dirt")
     else:
         print("Invalid cuisine choice")
 elif meal == "B":
-    food = input("American(A) or Indian(I): ")
+    food = input("American(A) or Italian (I): ")
     if food == "A":
-        choice = input("Pancakes or Waffles (enter full name case sensitive): ")
-        if choice == "Pancakes" or choice == "Waffles":
+        choice = input("Hot dog or Burger (enter full name case sensitive): ")
+        if choice == "Hotdog" or Burger == "Waffles":
             print("You got", choice)
         else:
             print("Eat dirt")
     elif food == "I":
-        choice = input("Paratha or Idli (enter full name case sensitive): ")
-        if choice == "Paratha" or choice == "Idli":
+        choice = input("Pizza or Pasta(enter full name case sensitive): ")
+        if choice == "Pizza" or choice == "Pasta":
             print("You got", choice)
         else:
             print("Eat dirt")
     else:
         print("Invalid cuisine choice")
 elif meal == "D":
-    food = input("Italian(I) or Chinese(C): ")
-    if food == "I":
-        choice = input("Pizza or Pasta (enter full name case sensitive): ")
-        if choice == "Pizza" or choice == "Pasta":
+    food = input("Korean (K) or Japanese (J): ")
+    if food == "K":
+        choice = input(" Soft Tofu Stew or Kimbap (enter full name case sensitive): ")
+        if choice == "Soft Tofu Stew" or choice == "Kimbap":
             print("You got", choice)
         else:
             print("Eat dirt")
-    elif food == "C":
-        choice = input("Noodles or Dumplings (enter full name case sensitive): ")
-        if choice == "Noodles" or choice == "Dumplings":
+    elif food == "J":
+        choice = input("Ramen or Shushi (enter full name case sensitive): ")
+        if choice == "Ramen" or choice == "Shushi":
             print("You got", choice)
         else:
             print("Eat dirt")
     else:
         print("Invalid cuisine choice")
 else:
-    print("Get out")
+    print("Sorry!")
